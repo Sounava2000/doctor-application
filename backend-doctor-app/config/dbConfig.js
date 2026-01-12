ad4b4158687212b512f1ab5d3e6711e3da25d46e
Sounava Mukherjee <soumo2640@gmail.com>
2026-01-12 15:52:45 +0530

    Fix MongoDB Atlas connection config

--- backend-doctor-app/config/dbConfig.js
+++ backend-doctor-app/config/dbConfig.js
@@ -1,18 +1,11 @@
- 
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const dbConnect = async () => {
   try {
-    const dbConfig = await mongoose.connect(process.env.mongodb_url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      tls: true
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.log("Error is:", error.message);
-    throw new Error("Error connecting to MongoDB");
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1);
   }
 };
